0e530d87045dc9bfc4254eadb2f6c900d0e56398
bsaid4323-design <bsaid4323@gmail.com>
2026-05-25 17:39:30 +0200

    Add files via upload
    Renewable Energy Engineering Portfolio showcasing my skills, projects, and education.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.style.boxShadow = '0 20px 30px rgba(0,0,0,0.1)';
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    target.scrollIntoView({
+      behavior:'smooth'
     });
-    card.addEventListener('mouseleave', () => {
-        card.style.boxShadow = '0 10px 20px rgba(0,0,0,0.05)';
-    });
+  });
 });
