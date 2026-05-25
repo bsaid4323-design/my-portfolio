370ecd411ae94d2601835305d916d058e06c9cf8
bsaid4323-design <bsaid4323@gmail.com>
2026-05-25 22:26:44 +0200

    Add files via upload
    Personal portfolio website showcasing my skills, education, and renewable energy projects.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 document.querySelectorAll('nav a').forEach(link => {
+
   link.addEventListener('click', function(e){
+
     e.preventDefault();
 
-    const target = document.querySelector(this.getAttribute('href'));
+    const target = document.querySelector(
+      this.getAttribute('href')
+    );
 
     target.scrollIntoView({
       behavior:'smooth'
     });
+
   });
+
 });
