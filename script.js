78dabbc1443c0f82d29869d2e23d29ab2fef894a
brahmi said <bsaid4323@gmail.com>
2026-05-28 20:49:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.querySelectorAll('nav a').forEach(link => {
+const toggle = document.getElementById("themeToggle");
 
-  link.addEventListener('click', function(e){
+toggle.onclick = () => {
+  document.body.classList.toggle("light");
+};
 
-    e.preventDefault();
-
-    const target = document.querySelector(
-      this.getAttribute('href')
-    );
-
-    target.scrollIntoView({
-      behavior:'smooth'
-    });
-
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
   });
+});
 
+document.querySelectorAll(".fade").forEach(el=>{
+  observer.observe(el);
 });
